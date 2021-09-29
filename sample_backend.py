dc62d9627eeb488e62b196655aee3aecc29682ab
jettcost38 <jecost2@gmail.com>
2021-09-29 09:27:04 -0700

    third commit

--- sample_backend.py
+++ sample_backend.py
@@ -2,15 +2,13 @@ from flask import Flask
 from flask import request
 app = Flask(__name__)
 
-@app.route('/users')
-def get_users():
-   search_username = request.args.get('name') #accessing the value of parameter 'name'
-   if search_username :
-      subdict = {'users_list' : []}
+@app.route('/users/<id>')
+def get_user(id):
+   if id :
       for user in users['users_list']:
-         if user['name'] == search_username:
-            subdict['users_list'].append(user)
-      return subdict
+        if user['id'] == id:
+           return user
+      return ({})
    return users
 
 users = { 
